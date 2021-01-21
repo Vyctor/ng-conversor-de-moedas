f4ad0de99f3d3b84c8680daee1ba51b169cfc5db
Vyctor <dev.vyctor@gmail.com>
2021-01-21 10:08:33 -0300

    ajustando testes unitarios

--- src/app/conversor/components/conversor/conversor.component.spec.ts
+++ src/app/conversor/components/conversor/conversor.component.spec.ts
@@ -1,7 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ConversorComponent } from './conversor.component';
 import { ConversorService, MoedaService } from '../../services';
+import { NumeroDirective } from '../../directives';
+import { DataBrPipe } from '../../pipes';
+import { ModalCotacaoComponent } from '../../components';
 
 describe('ConversorComponent', () => {
   let component: ConversorComponent;
@@ -9,8 +14,9 @@ describe('ConversorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ConversorComponent],
+      declarations: [ConversorComponent, NumeroDirective, DataBrPipe, ModalCotacaoComponent],
       providers: [MoedaService, ConversorService],
+      imports: [FormsModule, HttpClientModule]
     }).compileComponents();
   });
 
