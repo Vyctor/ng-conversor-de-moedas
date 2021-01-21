05a9d8ee6534227d01c861789c2e094294990538
Vyctor <dev.vyctor@gmail.com>
2021-01-20 21:28:55 -0300

    implementando a diretiva de validacao de numeros

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,10 +1,43 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Directive({
-  selector: '[appNumero]'
+  selector: '[appNumero]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumeroDirective,
+    multi: true
+  }]
 })
-export class NumeroDirective {
 
-  constructor() { }
+export class NumeroDirective implements ControlValueAccessor {
 
+  public onTouched: any;
+  public onChange: any;
+
+  constructor(private elementRef: ElementRef) { }
+
+  @HostListener('keyup', ['$event'])
+  public onKeyUp($event: any): void {
+    let valor = $event.target.value;
+    const posicaoDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if (posicaoDecimais > 0) {
+      valor = valor.substring(0, posicaoDecimais) + '.' + valor.substring(posicaoDecimais);
+    }
+    $event.target.value = valor;
+    this.onChange(valor);
+  }
+
+  writeValue(obj: any): void {
+    this.elementRef.nativeElement.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 }
